db69b4beaedc0c1eb4333aa35cc34a3308e9682c
William <william.marback@hotmail.com>
2020-09-29 10:54:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[20, 10];
+        }
+    }
+}
