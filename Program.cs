015df54a62dcdddfb4e65439c6bcf81b83a18d3d
William <william.marback@hotmail.com>
2020-09-30 11:11:00 +0200

    Jämnförelse

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace ConsoleSweeper
     {
         static void Main(string[] args)
         {
+            SweeperGrids sweeperGrids = new SweeperGrids();
+
+            sweeperGrids.CheckPosition();
+            
             Console.ReadLine();
         }
     }
--- SweeperGrids.cs
+++ SweeperGrids.cs
@@ -4,10 +4,15 @@ namespace ConsoleSweeper
 {
     public class SweeperGrids
     {
+        private Random random = new Random();
+        
+        private int[,] board = new int[10, 10];
+        private bool success = false;
+        private int playerChooseInt;
+        private int playerChooseInt2;
+        
         public SweeperGrids()
         {
-            int[,] board = new int[10, 10];
-
             for (int i = 0; i < board.GetLength(1); i++)
             {
                 for (int i2 = 0; i2 < board.GetLength(0); i2++)
@@ -16,14 +21,39 @@ namespace ConsoleSweeper
                 }
             }
 
+            for (int i = 0; i < 10; i++)
+            {
+                board[random.Next(board.GetLength(1)), random.Next(board.GetLength(0))] = 10;
+            }
+
             for (int i = 0; i < board.GetLength(1); i++)
             {
                 for (int i2 = 0; i2 < board.GetLength(0); i2++)
                 {
-                    Console.Write(board[i, i2]);
+                    Console.Write(board[i, i2]+" ");
                 }
+
                 Console.WriteLine();
             }
+        }
+
+        public void CheckPosition()
+        {
+            string playerChoose = Console.ReadLine();
+            success = int.TryParse(playerChoose, out playerChooseInt);
+
+            string playerChoose2 = Console.ReadLine();
+            success = int.TryParse(playerChoose2, out playerChooseInt2);
+
+            int[] tempArray = {playerChooseInt, playerChooseInt2};
+
+            if (board[playerChooseInt, playerChooseInt2] == 10)
+            {
+                System.Console.WriteLine("works");
+            }
+
+            //Debug-kod
+            Console.WriteLine(board[playerChooseInt, playerChooseInt2]);
         }
     }
 }
